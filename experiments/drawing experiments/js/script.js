39581c2dc985b0ca5ccfaa4d0509d8744f678854
MalikaIsimbi <malieizzy@gmail.com>
2020-09-16 22:29:52 -0400

    Drawing experiment: added color to my shapes

--- experiments/drawing experiments/js/script.js
+++ experiments/drawing experiments/js/script.js
@@ -9,7 +9,22 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(500,500);
 
+background(255,255,50);
+
+stroke(255,250,30);
+
+point(300,200);
+
+rect(100,100,250,250);
+fill(139,35,50);
+
+line(0,0,500,500);
+
+line(500,0,0,500);
+
+ellipse(250,250,100,100);
 }
 
 // draw()
