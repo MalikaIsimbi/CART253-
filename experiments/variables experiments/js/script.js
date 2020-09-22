b0e260430b10b44b48d4f053b504f2b7e24039db
MalikaIsimbi <malieizzy@gmail.com>
2020-09-21 22:48:42 -0400

    Learned about built-in variables

--- experiments/variables experiments/js/script.js
+++ experiments/variables experiments/js/script.js
@@ -1,20 +1,22 @@
 /**************************************************
 Template p5 project
-Isimbi Malika Kabagema 
+Isimbi Malika Kabagema
 
-Here is a description of this template p5 project.
+Experimenting with variables.
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+background(mouseX,mouseY,0);
+rectMode(CENTER);
+rect(mouseX,mouseY,100,100);
 }
