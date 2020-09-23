e842a114835e46fa16aa85ed6a4337c18cc02ff7
MalikaIsimbi <malieizzy@gmail.com>
2020-09-23 00:23:22 -0400

    Learned about random numbers, map and constrain :)

--- experiments/variables experiments/js/script.js
+++ experiments/variables experiments/js/script.js
@@ -8,11 +8,12 @@ Experimenting with variables.
 let backgroundShade = 0;
 
 let circle = {
-  x: 0,
+  x: 250,
   y: 250,
   size: 200,
   speed: 2,
-  acceleration: 0.25,
+  // acceleration: 0.25,
+  fill: 250,
 };
 // setup()
 //
@@ -27,11 +28,27 @@ createCanvas(500,500);
 // Description of draw() goes here.
 function draw() {
 background(backgroundShade);
-circle.x = circle.x + circle.speed;
-circle.speed = circle.speed + circle.acceleration;
+
+// circle.speed = random(-5,5);
+ circle.x = circle.x + circle.speed;
+// circle.y = random(0, height);
+// circle.size = random(500,50);
+// circle.speed = circle.speed + circle.acceleration;
 ellipse(circle.x, circle.y, circle.size);
 
-console.log(`circle,x: ${circle.x}, circle,y: ${circle.y}`);
+// let randomNumber = random();
+
+circle.size = map(mouseY,0,height,0,500);
+circle.fill = map(mouseX, 0,width, 0,255);
+circle.x = constrain(circle.x, 0,width);
+circle.y = constrain(circle.y, 0,height);
+
+// circle.fill = random(100,250);
+circle.fill = map(mouseX,0,width,0,255);
+fill(circle.fill);
+
+
+// console.log(`circle,x: ${circle.x}, circle,y: ${circle.y}`);
 // console.log("circle.y:" + circle.y);
 
 }
