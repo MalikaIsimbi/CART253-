759cd31925f582b65280c3f1867514f6f98dabbb
MalikaIsimbi <malieizzy@gmail.com>
2020-09-24 00:54:53 -0400

    E1: Created variables bg, alien1, alien2 and house
    bg will cover the colour of the canvas, alien1 and alien2 are the two characters, and house is a square that shows the inside of the house.

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -4,7 +4,35 @@ Isimbi Malika Kabagema
 
 This exercise is about two little sibling aliens, Perry and Lola, who live together in a colourful house and move around a lot around their house! They always seem to bump into each other!
 **************************************************/
+let bg = {
+  r:0,
+  g:0,
+  b:0,
+}
 
+let alien1 = {
+ x:
+ y:
+ size:
+ speed:
+ fill:
+}
+
+let alien2 = {
+  x:
+  y:
+  size:
+  speed:
+  fill:
+}
+
+let house = {
+  x:
+  y:
+  size:
+  fill:
+  
+}
 // setup()
 //
 // Description of setup() goes here.
