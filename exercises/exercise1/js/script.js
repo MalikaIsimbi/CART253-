9588e6f9e506b8a293fe38757d5da6c9c817a81f
MalikaIsimbi <malieizzy@gmail.com>
2020-09-24 00:47:22 -0400

    E1: Described the premise of the exercise and created a canvas.

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -2,14 +2,15 @@
 Ex.3: I like to move it move it!
 Isimbi Malika Kabagema
 
-Here is a description of this template p5 project.
+This exercise is about two little sibling aliens, Perry and Lola, who live together in a colourful house and move around a lot around their house! They always seem to bump into each other!
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(500,500);
+noStroke();
 }
 
 // draw()
