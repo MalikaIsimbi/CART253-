19bfe6ca69b63a7720b9b6d4d6b75040e6eaa70f
MalikaIsimbi <malieizzy@gmail.com>
2020-09-24 21:46:46 -0400

    E1: Trying to debug a variable

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -18,12 +18,26 @@ let alien1 = {
  width:75,
  height:100,
  alpha:10,
+
+ eye1:{
+   x:150,
+   y:200,
+   size:50,
+   fill:0,
+
+  eye2:{
+   x:200,
+   y:200,
+   size:50,
+   fill:0,
+
  fill:{
    r:75,
    g:235,
    b:86,
  }
-}
+
+ }
 
 let alien2 = {
   x:400,
@@ -33,18 +47,33 @@ let alien2 = {
   width:75,
   height:100,
   alpha:10,
+
+  eye1:{
+    x:300,
+    y:200,
+    size:50,
+    fill:0,
+
+  eye2: {
+    x:350,
+    y:200,
+    size:50,
+    fill:0,
+
   fill: {
     r:199,
     g:83,
     b:245,
   }
-}
+
+  }
 
 let house = {
   x:250,
   y:250,
   size:400,
-  speed:2,
+  biggerSize:4,
+  speed:10,
   width:400,
   height:400,
   alpha:100,
@@ -78,23 +107,34 @@ bg.r= map(mouseY,0, height,50,255);
 bg.g= map(mouseX,0, width,0,255);
 bg.b= map(mouseY,0, height,0,255);
 
+// This is the house perimeter.
+rectMode(CENTER);
+rect(house.x, house.y, house.width, house.height);
+fill(house.fill.r, house.fill.g, house.fill.b);
+house.size += house.biggerSize;
+// house.size = constrain(house.size,0,width);
+// house.size+= house.speed;
+// house.x = constrain(house.x,0,300);
+// house.x = map(house.x,0,width,0,300);
 
 // Alien1 (Perry)
 
 ellipse(alien1.x,alien1.y,alien1.width,alien1.height);
 fill(alien1.fill.r,alien1.fill.g,alien1.fill.b);
+// ellipse(alien1.eye1.x,alien1.eye1.y,alien1.eye1.size);
+// ellipse(alien1.eye2.x,alien1.eye2.y,alien1.eye2.size);
+// fill(alien1.eye1.fill);
+// fill(alien1.eye2.fill);
 
 // Alien2 (Lola)
 
 ellipse(alien2.x,alien2.y,alien2.width,alien2.height);
 fill(alien2.fill.r,alien2.fill.g,alien2.fill.b);
+// ellipse(alien2.eye1.x,alien2.eye1.y,alien2.eye1.size);
+// ellipse(alien2.eye2.x,alien2.eye2.y,alien2.eye2.size);
+// fill(alien2.eye1.fill);
+// fill(alien2.eye2.fill);
 
-// This is the house perimeter.
-rectMode(CENTER);
-rect(house.x, house.y, house.width, house.height);
-fill(house.fill.r, house.fill.g, house.fill.b);
-fill(house.alpha);
-
-
+console.log(alien2);
 
 }
