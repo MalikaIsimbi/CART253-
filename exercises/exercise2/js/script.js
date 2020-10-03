326403e2577b71b9260b8723ab0f9fec803b7df7
MalikaIsimbi <malieizzy@gmail.com>
2020-10-03 01:32:02 -0400

    E2: Set a colourful background, an alien creature named Malia that moves freely around
    Background changes colour with the mouseX,mouseY variables and the map function. The alien moves around with  the help of mouseX and mouse Y as well.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -1,14 +1,59 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+exercise-03-dodge-em
+Isimbi Malika Kabagema
 
-Here is a description of this template p5 project.
+This project is about catching/buying time in a colourful galaxy. The user(the little alien creature- Malia) is running out of time and trying to buy as much time as she can by catching the little time icons.
 **************************************************/
+let bg = {
+  r:0,
+  g:0,
+  b:0,
+};
+
+let alien = {
+  x:250,
+  y:250,
+  widthSize:50,
+  heightSize:55,
+  fill: {
+    r:131,
+    g:204,
+    b:86,
+  }
+};
+
+// This is to display the stars in the galaxy.
+let starsAmount = 500;
+
+// These are time icons that Malia the alien has to catch.
+let timeImage1 = {
+
+};
+let timeImage2 = {
+
+};
+let timeImage3 = {
+
+};
+let timeImage4 = {
+
+};
+
+// This helps to display the time icons in the canvas.
+function preload() {
+  timeImage1 = loadImage("assets/images/timeicon1.png");
+  timeImage2 = loadImage("assets/images/timeicon2.png");
+  timeImage3 = loadImage("assets/images/timeicon3.png");
+  timeImage4 = loadImage("assets/images/timeicon4.png");
+}
+
+
 
 // setup()
 //
-// Description of setup() goes here.
+// This is the canvas of the colourful galaxy.
 function setup() {
+createCanvas(windowWidth,windowHeight);
 
 }
 
@@ -16,5 +61,20 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+background(bg.r,bg.g,bg.b);
+
+bg.r= map(mouseX,0,width,140,200);
+bg.g= map(mouseX,0,width,0,200);
+bg.b= map(mouseY,0,height,50,255);
+
+// This helps Malia move around (to the left).
+alien.x = mouseX;
+alien.y = mouseY;
+
+// This is the little alien creature, Malia.
+fill(alien.fill.r,alien.fill.g,alien.fill.b);
+noStroke();
+ellipse(alien.x,alien.y,alien.widthSize,alien.heightSize);
 
 }
