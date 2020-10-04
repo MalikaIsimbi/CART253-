a7018ae233472faa2629d44921cbc0bf74c69d0f
MalikaIsimbi <malieizzy@gmail.com>
2020-10-03 20:49:59 -0400

    E2: changed the alien and added an actual image of the alien and made the original ellipse into the alien's shadow/halo.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -10,11 +10,10 @@ let bg = {
   b:0,
 };
 
-let alien = {
+let alienShadow = {
   x:250,
   y:250,
-  widthSize:50,
-  heightSize:55,
+  size:70,
   fill: {
     r:131,
     g:204,
@@ -42,6 +41,10 @@ let timeImage4 = {
 x:0,
 y:0,
 };
+let maliaImage = {
+x:250,
+y:250,
+}
 
 // This helps to display the time icons in the canvas.
 function preload() {
@@ -49,6 +52,9 @@ function preload() {
   timeImage2 = loadImage("assets/images/timeicon2.png");
   timeImage3 = loadImage("assets/images/timeicon3.png");
   timeImage4 = loadImage("assets/images/timeicon4.png");
+
+  // This will help to display Malia, the alien.
+  maliaImage = loadImage("assets/images/malia.png");
 }
 
 // setup()
@@ -56,6 +62,8 @@ function preload() {
 // This is the canvas of the colourful galaxy.
 function setup() {
 createCanvas(windowWidth,windowHeight);
+
+noCursor();
 }
 
 // draw()
@@ -66,11 +74,12 @@ function draw() {
 // the actual galaxy being drawn here.
 background(bg.r,bg.g,bg.b);
 
+// this is creating the colourful galaxy effect.
 bg.r= map(mouseX,0,width,140,150);
 bg.g= map(mouseX,0,width,50,150);
 bg.b= map(mouseY,0,height,50,255);
 
-// drawing the stars in the galaxy.
+// drawing the stars(points) in the galaxy.
 for (let i = 0; i < starsAmount; i ++) {
   let x = random(0,width);
   let y = random(0,height);
@@ -78,19 +87,23 @@ for (let i = 0; i < starsAmount; i ++) {
   point(x,y);
 }
 
+// This helps Malia's shadow move around.
+alienShadow.x = mouseX;
+alienShadow.y = mouseY;
+
+// This is Malia's shadow\halo, the green ellipse.
+fill(alienShadow.fill.r,alienShadow.fill.g,alienShadow.fill.b);
+noStroke();
+ellipse(alienShadow.x,alienShadow.y,alienShadow.size);
+
+// Malia the alien is being drawn here.
+image(maliaImage,mouseX,mouseY,100,100);
+
 // the four time icons are being drawn here.
 image(timeImage1,0,0,100,100);
 image(timeImage2,0,150,100,100);
 image(timeImage3,0,300,70,70);
 image(timeImage4,0,450,100,100);
 
-// This helps Malia move around.
-alien.x = mouseX;
-alien.y = mouseY;
-
-// This is the little alien creature, Malia.
-fill(alien.fill.r,alien.fill.g,alien.fill.b);
-noStroke();
-ellipse(alien.x,alien.y,alien.widthSize,alien.heightSize);
 
 }
