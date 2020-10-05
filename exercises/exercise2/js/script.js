f7aac5dc6a8382bb824afb1e6150b6dce5097797
MalikaIsimbi <malieizzy@gmail.com>
2020-10-04 21:52:10 -0400

    E2: Modified the mouseX,mouseY function in order for the if statement to function (I added them in order to have the time icons change tints when they come in contact with Malia).
    Still fixing some issues on the tint(), also added push() and pop() in order to keep the shapes intact.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -13,7 +13,7 @@ let bg = {
 let alienShadow = {
   x:250,
   y:250,
-  size:50,
+  size:100,
   fill: {
     r:131,
     g:204,
@@ -37,7 +37,7 @@ tint: {
   r:119,
   g:201,
   b:68,
-  alpha:100,
+  alpha:50,
 },
 speed:3,
 };
@@ -53,7 +53,7 @@ tint: {
   r:119,
   g:201,
   b:68,
-  alpha:100,
+  alpha:50,
 },
 speed:3,
 };
@@ -69,7 +69,7 @@ tint:{
   r:119,
   g:201,
   b:68,
-  alpha:100,
+  alpha:50,
 },
 speed:3,
 };
@@ -85,7 +85,7 @@ tint: {
   r:119,
   g:201,
   b:68,
-  alpha:100,
+  alpha:50,
 },
 speed:3,
 };
@@ -115,6 +115,7 @@ function preload() {
 // This is the canvas of the colourful galaxy.
 function setup() {
 createCanvas(500,500);
+imageMode(CENTER);
 
 // This is helping the time icons to move up and down.
 time1.y = random(0,height);
@@ -150,9 +151,11 @@ for (let i = 0; i < starsAmount; i ++) {
   point(x,y);
 }
 
-// This helps Malia's shadow move around.
+// This helps Malia's shadow and Malia to move around.
 alienShadow.x = mouseX;
 alienShadow.y = mouseY;
+malia.x = mouseX;
+malia.y = mouseY;
 
 // This is to enable the time icons' movement.
 time1.x += time1.vx;
@@ -167,16 +170,16 @@ time4.y += time4.vy;
 // This is for the time icons to move up and down repeatedly.
 if (time1.y > height) {
   time1.y = 0;
-}
+};
 if (time2.y < -100) {
   time2.y = height;
-}
+};
 if (time3.y > height) {
   time3.y = 0;
-}
+};
 if (time4.y < -100) {
   time4.y = height;
-}
+};
 
 // the four time icons are being drawn here.
 image(time1.image,time1.x,time1.y,time1.w,time1.h);
@@ -184,32 +187,56 @@ image(time2.image,time2.x,time2.y,time2.w,time2.h);
 image(time3.image,time3.x,time3.y,time3.w,time3.h);
 image(time4.image,time4.x,time4.y,time4.w,time4.h);
 
-// Malia the alien is being drawn here.
-image(malia.image,mouseX,mouseY,malia.w,malia.h);
-
 // This is Malia's shadow\halo, the green ellipse.
+push();
 fill(alienShadow.fill.r,alienShadow.fill.g,alienShadow.fill.b);
 noStroke();
 ellipse(alienShadow.x,alienShadow.y,alienShadow.size);
+pop();
+
+// Malia the alien is being drawn here.
+image(malia.image,malia.x,malia.y,malia.w,malia.h);
 
 // The tint (green disappearing effect) of the time icons is being drawn here
-// tint(time1.tint.r,time1.tint.g,time1.tint.b,time1.tint.alpha);
-// tint(time2.tint.r,time2.tint.g,time2.tint.b,time2.tint.alpha);
-// tint(time3.tint.r,time3.tint.g,time3.tint.b,time3.tint.alpha);
-// tint(time4.tint.r,time4.tint.g,time4.tint.b,time4.tint.alpha);
+push();
+tint(time1.tint.r,time1.tint.g,time1.tint.b,time1.tint.alpha);
+tint(time2.tint.r,time2.tint.g,time2.tint.b,time2.tint.alpha);
+tint(time3.tint.r,time3.tint.g,time3.tint.b,time3.tint.alpha);
+tint(time4.tint.r,time4.tint.g,time4.tint.b,time4.tint.alpha);
+pop();
 
-// The time icons are being caught by Malia here.
+// The time icons are being caught by Malia here, as well as the tint modification is taking place.
 
 let d1 = dist(time1.x,time1.y,malia.x,malia.y);
 let d2 = dist(time2.x,time2.y,malia.x,malia.y);
 let d3 = dist(time3.x,time3.y,malia.x,malia.y);
 let d4 = dist(time4.x,time4.y,malia.x,malia.y);
 
-if (d1 < time1.w / 2 + malia.w / 2) {
-  noLoop();
-}
 
-// The tint of the time icons is being modified here once they are caught (green effect).
+if (d1 < time1.w / 2 + malia.w / 2) {
+tint(time1.tint.r,time1.tint.g,time1.tint.b,time1.tint.alpha);
+} else {
+  noTint();
+};
+ if (d2 > time2.w / 2 + malia.w / 2) {
+tint(time2.tint.r,time2.tint.g,time2.tint.b,time2.tint.alpha);
+} else {
+  noTint();
+};
+if (d3 < time3.w / 2 + malia.w / 2) {
+tint(time3.tint.r,time3.tint.g,time3.tint.b,time3.tint.alpha);
+} else {
+ noTint();
+};
+if (d4 < time4.w / 2 + malia.w / 2) {
+tint(time4.tint.r,time4.tint.g,time4.tint.b,time4.tint.alpha);
+} else {
+ noTint();
+};
+
+
+
+
 
 
 }
