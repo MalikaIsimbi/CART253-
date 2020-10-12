4bdd98c6331ebe9e38d017e19001a75f214f335c
MalikaIsimbi <malieizzy@gmail.com>
2020-10-11 20:53:29 -0400

    E3: Included all the images that will show up in the canvas.

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+exercise-03-love-actually
+Isimbi Malika Kabagema
 
-Here is a description of this template p5 project.
+This exercise is about my undying love for the famous actor, Timothée Chalamet, who doesn't know I exist, hence the love being unrequited. The user, Malika, will be chasing him around a museum in Paris until she realizes that it's an illusion, and she needs to find love...within herself! That's the happy ending. The sad ending, well, she juat realizes it's an illusion and ends up sad. 
 **************************************************/
 
 // setup()
