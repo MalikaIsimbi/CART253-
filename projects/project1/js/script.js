5c6bdbebb42487e349a582927016ed5a91500119
MalikaIsimbi <malieizzy@gmail.com>
2020-10-15 02:36:09 -0400

    P1: Added description of project and added images to be used.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,17 +1,142 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+project-1: Escaping the Matrix.
+Isimbi Malika Kabagema.
 
-Here is a description of this template p5 project.
+This project is called Escaping the Matrix.
+ It's a game about a girl who is stuck in the matrix and has to go through multiple levels to escape. There are 3 levels: the social media realm, the society realm and the otherness (face yourself) realm.
+ She completes each level by avoiding these traps and making it to the next state. If she fails one level, she is able to restart that level and continue till the end.
+ Once she completes these 3 levels, she is freed from the matrix and starts her journey to true freedom. The idea being this is to show how we are all somehow lost in a world full of misinformation, how social media and society norms tend to dictate our lives, how we also limit ourselves and self-sabotage and how most of us don't even realize it, hence I made this project to address that and tell myself and others to confront these issues and be the best version of ourselves and not let external factors derail that.
 **************************************************/
+let socialMediaGirl = {
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+  speed: 5,
+};
+
+let societyGirl = {
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+  speed: 5,
+};
+
+let othernessGirl = {
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+  speed: 5,
+};
+
+let almostFreeGirl = {
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+  speed: 5,
+};
+
+let originalGirl = {
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: 0,
+  vy: 0,
+  image: undefined,
+  speed: 5,
+};
+
+let matrixEntry = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let matrixFail = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let instagram = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let youtube = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let facebook = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let pinterest = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let twitter = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let snapchat = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let society1 = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let society2 = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+};
+
+let freedom = {
+  x: undefined,
+  y: undefined,
+  image: undefined,
+}
+
+
 
 // setup()
 //
-// Description of setup() goes here.
+// The canvas is created here.
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
