908036377c31bba1d7af238f2e94ff9cad7bf184
MalikaIsimbi <malieizzy@gmail.com>
2020-10-15 16:30:38 -0400

    P1: Trying to solve console issue.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -78,9 +78,9 @@ let matrixFail = {
 
 let instagram = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 100,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -89,9 +89,9 @@ let instagram = {
 
 let youtube = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 150,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -100,9 +100,9 @@ let youtube = {
 
 let facebook = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 200,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -111,9 +111,9 @@ let facebook = {
 
 let pinterest = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 300,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -122,9 +122,9 @@ let pinterest = {
 
 let twitter = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 350,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -133,9 +133,9 @@ let twitter = {
 
 let snapchat = {
   x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
+  y: 400,
+  w: 50,
+  h: 50,
   vx: 0,
   vy: 0,
   image: undefined,
@@ -170,7 +170,10 @@ let freedom = {
   image: undefined,
 };
 
-let state = `enterMatrix`; //
+let staticAmount = 500;
+
+let state = `enterMatrix`;
+
 
 // This is to display all of the images on the canvas.
 function preload() {
@@ -200,6 +203,8 @@ function preload() {
 // The canvas is created here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  setUpObjects();
 }
 
 // draw()
@@ -207,69 +212,159 @@ function setup() {
 // The images, character and different simulation levels are being drawn here.
 function draw() {
 
-if (state === `enterMatrix`) {
-  startMatrix();
-} else if (state === `matrixLevel1`) {
-  level1();
-} else if (state === `matrixLevel2`) {
-  level2();
-} else if (state === `matrixLevel3`) {
-  level3();
-} else if (state === `endGame`){
-  endGame();
-}
-
-}
-
-// Intro page of the game
-function startMatrix(){
-  // Display background of matrix.
-  image(matrixEntry.image, windowWidth, windowHeight);
-  background(matrixEntry.image);
-  // Display title.
-  push();
-  textSize(51);
-  fill(255,255,255);
-  textAlign(CENTER, CENTER);
-  textFont(`Russo One`);
-  text(`Welcome to the Matrix...`, width / 2, height / 2);
-  textSize(50);
-  fill(95,191,6);
-  textAlign(CENTER, CENTER);
-  textFont(`Russo One`);
-  text(`Welcome to the Matrix...`, width / 2, height / 2);
-  pop();
-  // Display subtitle.
-  push();
-  textSize(30);
-  fill(95,191,6);
-  textAlign(CENTER,BASELINE);
-  textFont(`Play`);
-  text(`Will you manage to escape?`, width/2, 400);
-  pop();
-  // Title to start game.
-  push();
-  textSize(41);
-  fill(255,255,255);
-  textAlign(CENTER,BOTTOM);
-  textFont(`Russo One`);
-  text(`CLICK TO START.`, width/2, 550);
-  textSize(40);
-  fill(95,191,6);
-  textAlign(CENTER,BOTTOM);
-  textFont(`Russo One`);
-  text(`CLICK TO START.`, width/2, 550);
-  pop();
-}
-
-function mousePressed() {
-  // Once the mouse is pressed, the game begins.
   if (state === `enterMatrix`) {
-    state = `matrixLevel1`;
+    startMatrix();
+  } else if (state === `matrixLevel1`) {
+    level1();
+  } else if (state === `matrixLevel2`) {
+    level2();
+  } else if (state === `matrixLevel3`) {
+    level3();
+  } else if (state === `endGame`) {
+    endGame();
+  } else if (state === `winGame`) {
+    gameWon();
   }
+
 }
 
-// The window will adapt to whatever size you set it to.
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+function setUpObjects() {
+  // Placing the girl in a given position.
+  socialMediaGirl.x = width / 3;
+  societyGirl.x = width / 3;
+  othernessGirl.x = width / 3;
+  almostFreeGirl.x = width / 3;
+  originalGirl.x = width / 3;
+  // Placing the social media icons in a given position.
+  instagram.x = 2 * width / 3;
+  facebook.x = 2 * width / 3;
+  youtube.x = 2 * width / 3;
+  twitter.x = 2 * width / 3;
+  pinterest.x = 2 * width / 3;
+  snapchat.x = 2 * width / 3;
+  // Drawing the static in the matrix.
+  for (let i = 0; i < staticAmount; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(200);
+    point(x, y);
+  }
+
+  // Intro page of the game.
+  function startMatrix() {
+    // Display background of matrix.
+    image(matrixEntry.image, windowWidth, windowHeight);
+    background(matrixEntry.image);
+    // Display title.
+    push();
+    textSize(51);
+    fill(255, 255, 255);
+    textAlign(CENTER, CENTER);
+    textFont(`Russo One`);
+    text(`Welcome to the Matrix...`, width / 2, height / 2);
+    textSize(50);
+    fill(95, 191, 6);
+    textAlign(CENTER, CENTER);
+    textFont(`Russo One`);
+    text(`Welcome to the Matrix...`, width / 2, height / 2);
+    pop();
+    // Display subtitle.
+    push();
+    textSize(30);
+    fill(95, 191, 6);
+    textAlign(CENTER, BASELINE);
+    textFont(`Play`);
+    text(`Will you manage to escape?`, width / 2, 400);
+    pop();
+    // Title to start.
+    push();
+    textSize(41);
+    fill(255, 255, 255);
+    textAlign(CENTER, BOTTOM);
+    textFont(`Russo One`);
+    text(`CLICK TO START.`, width / 2, 550);
+    textSize(40);
+    fill(95, 191, 6);
+    textAlign(CENTER, BOTTOM);
+    textFont(`Russo One`);
+    text(`CLICK TO START.`, width / 2, 550);
+    pop();
+  }
+
+  function level1() {
+    simulationMatrix1();
+  }
+
+  // function level2() {
+  //   simulationMatrix2();
+  // }
+  //
+  // function level3() {
+  //   simulationMatrix3();
+  // }
+  //
+  // function endGame() {
+  //
+  // }
+  //
+  // function gameWon() {
+  //
+  // }
+
+  function simulationMatrix1() {
+    movementLevel1();
+    resetLevel1();
+    displayLevel1();
+  }
+
+  function movementLevel1() {
+    // Controlling the girl's movement.
+    socialMediaGirl.x = mouseX;
+    socialMediaGirl.y = mouseY;
+    // Controlling the social media icons' movement.
+    instagram.x += instagram.vx;
+    facebook.x += facebook.vx;
+    youtube.x += youtube.vx;
+    twitter.x += twitter.vx;
+    pinterest.x += pinterest.vx;
+    snapchat.x += snapchat.vx;
+  }
+
+  function displayLevel1() {
+    // Display background of level 1.
+    image(matrixEntry.image, windowWidth, windowHeight);
+    background(matrixEntry.image);
+    //Title of level 1.
+    // push();
+    // textSize(40);
+    // fill(95, 191, 6);
+    // textAlign(CENTER,TOP);
+    // textFont(`Russo One`);
+    // text(`Level 1: The Social Media Realm.`, width / 2, 550);
+    // pop();
+    // Display girl.
+    image(socialMediaGirl.image, socialMediaGirl.x, socialMediaGirl.y, socialMediaGirl.w, socialMediaGirl.h);
+    // Display social media icons.
+    image(instagram.image, instagram.x, instagram.y, instagram.w, instagram.h);
+    image(facebook.image, facebook.x, facebook.y, facebook.w, facebook.h);
+    image(youtube.image, youtube.x, youtube.y, youtube.w, youtube.h);
+    image(twitter.image, twitter.x, twitter.y, twitter.w, twitter.h);
+    image(pinterest.image, pinterest.x, pinterest.y, pinterest.w, pinterest.h);
+    image(snapchat.image, snapchat.x, snapchat.y, snapchat.w, snapchat.h);
+  }
+
+  // function resetLevel1() {
+  //
+  // }
+
+  // Once the mouse is pressed, the game begins.
+  function mousePressed() {
+    if (state === `enterMatrix`) {
+      state = `matrixLevel1`;
+    }
+  }
+
+  // The window will adapt to whatever size you set it to.
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
