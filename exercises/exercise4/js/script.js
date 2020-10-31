c9a64dba54d0bc6f9a489438743ff963af53864b
MalikaIsimbi <malieizzy@gmail.com>
2020-10-31 17:31:18 -0400

    E4: Started exercise 4

---
+++ exercises/exercise4/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+04-age-of-aquariums
+Isimbi Malika Kabagema
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
