4daf6f589f6f664312be01aff988bba391c38ced
MalikaIsimbi <malieizzy@gmail.com>
2020-11-06 23:29:21 -0500

    A4: Finished activity.

---
+++ activities/08-object-oriented-programming-activity/js/Paddle.js
@@ -0,0 +1,21 @@
+class Paddle {
+  constructor(w, h) {
+    this.x = 0;
+    this.y = height - this.height / 2;
+    this.width = w;
+    this.height = h;
+  }
+
+  move() {
+    this.x = mouseX;
+  }
+
+  display() {
+    push();
+    fill(138, 108, 94);
+    noStroke();
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
--- activities/08-object-oriented-programming-activity/js/script.js
+++ activities/08-object-oriented-programming-activity/js/script.js
@@ -1,22 +1,53 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+08-object-oriented-programming-activity
+Isimbi Malika Kabagema
+
 
 Here is a description of this template p5 project.
 **************************************************/
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 5;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-300, -100);
+    let ball = new Ball(x, y);
+    balls.push();
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(200);
 
+  paddle.move();
+  paddle.display(paddle);
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
+  // console.log(Paddle.js);
+  // console.log(Ball.js);
 }
